315fda9cb76d28ea1cd359eab221517c7ed6c58c
Gryffith <adamq94@wp.pl>
2025-05-27 17:13:54 +0200

    Clean up project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,11 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('angular-starter-kit');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-starter-kit');
-  });
 });
